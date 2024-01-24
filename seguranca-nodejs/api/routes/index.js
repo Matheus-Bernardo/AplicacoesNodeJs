8dd42ed15f6e9c04f33177a2b8622d374131e90f
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-01-23 23:22:10 -0300

    primeiro commit

---
+++ seguranca-nodejs/api/routes/index.js
@@ -0,0 +1,17 @@
+const bodyParser = require('body-parser')
+const produto = require('./produtoRoute')
+const usuario = require('./usuariosRoute')
+const auth = require('./authRoute')
+const role = require('./role')
+const permissao = require('./permissao')
+
+module.exports = app => {
+  app.use(
+    bodyParser.json(),
+    auth,
+    produto,
+    usuario,
+    role,
+    permissao
+  )
+}
