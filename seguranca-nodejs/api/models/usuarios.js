64439d2bd38b0ad7bb128fd7b5a7fd08ae2eae19
Matheus Henrique Lourenço Bernardo <matheus.bernardo@inatel.br>
2024-01-27 10:42:49 -0300

    relacionamentos db

--- seguranca-nodejs/api/models/usuarios.js
+++ seguranca-nodejs/api/models/usuarios.js
@@ -9,8 +9,18 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
+    //função responsável pelos relacionamentos dos banco de dados
     static associate(models) {
-      // define association here
+      usuarios.belongsToMany(models.roles,{
+        through: models.usuarios_roles,
+        as: 'usuario_roles',
+        foreignKey: 'usuario_id'
+      })
+      usuarios.belongsToMany(models.permissoes,{
+        through: models.usuarios_permissoes,
+        as: 'usuario_permissoes',
+        foreignKey: 'usuario_id'
+      })
     }
   }
   usuarios.init({
